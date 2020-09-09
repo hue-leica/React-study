c11bbb5a75919bd5845a338d86c4414bf6149fbd
jungwook <neity16@daum.net>
2020-09-10 02:23:26 +0900

    2020-09-10 개인정보 보호 처리

--- node-react(JohnAhn)/index.js
+++ node-react(JohnAhn)/index.js
@@ -1,14 +1,33 @@
 const express =require('express');
 const app = express();
 const port = 5000;
-
+const bodyParser = require('body-parser');
+const { User } = require('./models/user');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cheeze:cheeze123@node-react.dykzq.mongodb.net/<dbname>?retryWrites=true&w=majority',
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+const config = require('./config/key');
+mongoose.connect(config.mongoURI,
 {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...!'))
    .catch(err => console.log(err));
    
-app.get('/',(req,res)=> res.send('Hello world!'));
+app.get('/',(req,res)=> res.send('Hello world!!'));
+
+app.post('/register', (req,res) => {
+
+    const user = new User(req.body);
+    user.save((err, userInfo)=>{
+        if(err) return res.json({success: false, err});
+    return res.status(200).json({
+        success: true
+    });
+    });
+});
+
+
+
+
 app.listen(port,()=>console.log(`example app listening on port! ${port}`));
 
